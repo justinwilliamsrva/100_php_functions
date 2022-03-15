b55c91e79677360a6f4de9d8a5ed0e5bf50cf24b
Justin Williams <justinwdev@gmail.com>
2022-03-15 16:01:01 -0400

    explode

--- 1-10.php
+++ 1-10.php
@@ -7,6 +7,9 @@ $array = ['wolf', 'dog', 'cat', 'horse', 'frog'];
 $multi_array = ['wolf', 'dog', 'cat', 'horse', 'frog', ['wolf', 'sheep','rooster']];
 $sentence = "Clever Animals are Fast";
 $num_array = [1.0, 1.5, 2.0, 2.5];
+$comma_string = 'walk,try,hurt';
+$column_string = 'walk|try|hurt';
+$space_string = 'walk try hurt';
 
 
 //******************************************
@@ -49,7 +52,15 @@ $num_array = [1.0, 1.5, 2.0, 2.5];
 //******************************************
 
 // echo in_array('1.0', $num_array);
-echo in_array('1.0', $num_array,TRUE);
+// echo in_array('1.0', $num_array,TRUE);
+
+//******************************************
+//********** 5. explode  **********
+//******************************************
+
+// print_r(explode(",",$comma_string));
+// print_r(explode("|",$column_string,2));
+// print_r(explode(" ",$space_string,-1));
 
 
 
