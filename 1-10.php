ad055cf006130cb44c145d1dd023374dbf971204
Justin Williams <justinwdev@gmail.com>
2022-03-15 14:41:05 -0400

    Lesson 1

---
+++ 1-10.php
@@ -0,0 +1,19 @@
+<?php
+//********** 1. COUNT **********
+$array = ['wolf', 'dog', 'cat', 'horse', 'frog'];
+$multi_array = ['wolf', 'dog', 'cat', 'horse', 'frog', ['wolf', 'sheep','rooster']];
+// echo count($array);
+
+// for($i=0;$i<count($array)-1;$i++){
+//     printf($array[$i]."\n");
+// }
+
+// echo count($multi_array,COUNT_NORMAL);
+// echo "\n";
+// echo count($multi_array,COUNT_RECURSIVE);
+
+
+//********** 2. COUNT **********
+
+
+
